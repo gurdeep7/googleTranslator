7c8075f8624cd47c02ec1d38813068fed181f4bb
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2021-10-27 07:57:20 +0530

    done

--- index.js
+++ index.js
@@ -19,12 +19,54 @@ recognition.onresult = function(event) {
     var iLanguage = document.getElementById("iLanguage")
     var transcript = event.results[0][0].transcript;
     console.log(transcript);
-    iLanguage.textContent = transcript;
+    document.getElementById("cLanguage").value = "en"
+    iLanguage.value = transcript;
+    convert();
 };
-   function speak() {      
+   function speak() {  
+    let target = document.getElementById("oLanguage").value;  
+       var conLanguage = document.getElementById("conLanguage").value;
 var msg = new SpeechSynthesisUtterance();
-msg.lang = "hi"
+msg.lang = target;
 
-msg.text = "welcome";
+msg.text = conLanguage;
 window.speechSynthesis.speak(msg);
    }
+
+async function convert(){
+    var conLanguage = document.getElementById("conLanguage")
+    let q = document.getElementById("iLanguage").value
+    if(q.length == 0){
+        return;
+    }
+    console.log(q)
+    let source  = document.getElementById("cLanguage").value
+    let target = document.getElementById("oLanguage").value;
+    
+    data ={
+        q,
+        source,
+        target
+    }
+    data = JSON.stringify(data);
+    
+  let res = await fetch("https://libretranslate.de/translate", {
+	method: "POST",
+	body: data,
+	headers: { "Content-Type": "application/json" }
+});
+
+let targetLanguage =  await res.json()
+console.log(targetLanguage)
+conLanguage.value = targetLanguage.translatedText;
+}
+
+var timerId;
+function debounce(func,delay){
+    if(timerId){
+        clearTimeout(timerId)
+    }
+    timerId = setTimeout(() => {
+func()
+    }, delay);
+}
